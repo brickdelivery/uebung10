2e06e566b67dd4fd64ac834b25f60c837ae4bf42
brickdelivery <jburgma2@smail.uni-koeln.de>
2026-01-02 16:20:40 +0100

    remove tests

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,10 +1,6 @@
 package h1;
 
 public class H1_main {
-    public static void main(String[] args){
-        //tests
-        Cell[] cells = new Cell[]{new Cell(1,3),new Cell(2,3),new Cell(2,2)};
-        Grid g = new Grid(cells, 4,5);
-        g.outputGrid();
+    public static void main(String[] args) {
     }
 }
