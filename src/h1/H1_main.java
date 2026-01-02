2228cc516aeb2505325b2351c34d40df07e8533e
brickdelivery <jburgma2@smail.uni-koeln.de>
2026-01-02 15:08:02 +0100

    tests

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -3,9 +3,8 @@ package h1;
 public class H1_main {
     public static void main(String[] args){
         //tests
-        Cell[] cells = new Cell[]{new Cell(1,3)};
+        Cell[] cells = new Cell[]{new Cell(1,3),new Cell(2,3),new Cell(2,2)};
         Grid g = new Grid(cells, 4,5);
         g.outputGrid();
-
     }
 }
